5cb68f44d52014270a1d5084f220e7651d7a6d98
Stefan Papazov <spapazov@seas.upenn.edu>
2021-08-11 17:22:24 +0300

    finish card templates

--- angular/src/app/modules/popup/popup.module.ts
+++ angular/src/app/modules/popup/popup.module.ts
@@ -4,9 +4,17 @@ import { PopupComponent } from './pages/popup/popup.component';
 import { PopupRoutingModule } from './popup-routing.module';
 import { FooterComponent } from './pages/popup/footer/footer.component';
 import { HeaderComponent } from './pages/popup/header/header.component';
+import {CardComponent} from './pages/popup/card/card.component';
+import {CardsComponent} from './pages/popup/cards/cards.component';
 
 @NgModule({
-  declarations: [PopupComponent, FooterComponent, HeaderComponent],
+  declarations: [
+    PopupComponent,
+    FooterComponent,
+    HeaderComponent,
+    CardComponent,
+    CardsComponent
+  ],
   imports: [CommonModule, PopupRoutingModule]
 })
 export class PopupModule {}
